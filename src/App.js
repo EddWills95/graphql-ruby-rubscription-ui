edba365106a5253f79a0ebb038b20784eaf1f8bf
EddWills95 <edd.williams@me.com>
2018-08-07 22:27:36 +0100

    Add query to see if it works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { Query, ApolloProvider } from 'react-apollo';
+
+const GET_COUNTER = gql`
+  {
+    counter(id: 1) {
+      id
+      value
+    }
+  }
+`
+
+const counter = ({ onCounter }) => (
+  <Query query={GET_COUNTER}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <p>{data.counter.value}</p>
+      );
+    }}
+  </Query>
+);
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.client = new ApolloClient({
+      uri: 'http://localhost:3000/graphql'
+    });
+
+    this.subscribe = this.subscribe.bind(this);
+  }
+
+  subscribe() {
+    this.client.subscribe() 
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={this.client}>
+        <div className="App">
+        <button onClick={this.subscribe}>Subscribe</button>
+        <Query query={GET_COUNTER}>
+          {({data}) => {
+            console.log(data);
+            return <p>hey</p>
+          }}
+        </Query>
+        </div>
+      </ApolloProvider>
+      
     );
   }
 }
